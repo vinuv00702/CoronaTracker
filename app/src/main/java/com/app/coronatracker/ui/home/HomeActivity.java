15753cb28c40423b49c1e7596ea8ae6aafd63ab1
vinuvijayan <vinu00702@gmail.com>
2020-03-28 20:56:44 +0530

    project files added

---
+++ app/src/main/java/com/app/coronatracker/ui/home/HomeActivity.java
@@ -0,0 +1,81 @@
+package com.app.coronatracker.ui.home;
+
+import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.Switch;
+
+import com.app.coronatracker.R;
+import com.app.coronatracker.ui.dashboard.DashboardFragment;
+import com.app.coronatracker.ui.notifications.NotificationsFragment;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
+
+public class HomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+        BottomNavigationView navView = findViewById(R.id.nav_view);
+        navView.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+
+        loadFragment (new HomeFragment());
+        // Passing each menu ID as a set of Ids because each
+        // menu should be considered as top level destinations.
+        AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
+                R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications)
+                .build();
+//        NavController navController = Navigation.findNavController(this, R.id.mobile_navigation);
+//        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
+//        NavigationUI.setupWithNavController(navView, navController);
+    }
+
+    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
+            = new BottomNavigationView.OnNavigationItemSelectedListener() {
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+            Fragment fragment;
+
+            switch (menuItem.getItemId()) {
+                case R.id.nav_global:
+
+                    fragment = new HomeFragment();
+                    loadFragment(fragment);
+                    return true;
+
+                case R.id.nav_countries:
+
+                    fragment = new DashboardFragment();
+                    loadFragment(fragment);
+                    return true;
+
+                case R.id.nav_info:
+
+                    fragment = new NotificationsFragment();
+                    loadFragment(fragment);
+                    return true;
+            }
+
+            return false;
+        }
+    };
+
+
+    private void loadFragment(Fragment fragment) {
+
+        // load fragment
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+        transaction.replace(R.id.frame_container, fragment);
+        transaction.addToBackStack(null);
+        transaction.commit();
+    }
+
+}
---
+++ app/src/main/java/com/app/coronatracker/ui/home/HomeViewModel.java
@@ -0,0 +1,19 @@
+package com.app.coronatracker.ui.home;
+
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
+public class HomeViewModel extends ViewModel {
+
+    private MutableLiveData<String> mText;
+
+    public HomeViewModel() {
+        mText = new MutableLiveData<>();
+        mText.setValue("0000000");
+    }
+
+    public LiveData<String> getText() {
+        return mText;
+    }
+}
