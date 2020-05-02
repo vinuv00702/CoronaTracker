5db570275e21e1aae6745067417afc0496f0ed8f
Vishnu <vvijayan@e-cobalt2019>
2020-05-02 14:07:21 -0400

    Hash map added for statewise Api

--- app/src/main/java/com/app/coronatracker/ui/api/IndianData/IndianDataWebService.java
+++ app/src/main/java/com/app/coronatracker/ui/api/IndianData/IndianDataWebService.java
@@ -13,6 +13,7 @@ import com.google.gson.annotations.Expose;
 import com.google.gson.annotations.SerializedName;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 
 import retrofit2.Call;
 import retrofit2.Callback;
@@ -27,37 +28,39 @@ public class IndianDataWebService implements IndianDataWebInterface {
         @Expose
         private String key_values;
 
-        @SerializedName("total_values")
-        @Expose
-        private String total_values;
+//        @SerializedName("total_values")
+//        @Expose
+//        private String total_values;
 
         @SerializedName("state_wise")
         @Expose
-        private ArrayList<State> states;
+        private HashMap<String, State> states = new HashMap<String, State>();;
 
         public String getKey_values() {
             return key_values;
+        }
+
+        public HashMap<String, State> getStates() {
+            return states;
+        }
+
+        public void setStates(HashMap<String, State> states) {
+            this.states = states;
         }
 
         public void setKey_values(String key_values) {
             this.key_values = key_values;
         }
 
-        public String getTotal_values() {
-            return total_values;
-        }
+//        public String getTotal_values() {
+//            return total_values;
+//        }
+//
+//        public void setTotal_values(String total_values) {
+//            this.total_values = total_values;
+//        }
 
-        public void setTotal_values(String total_values) {
-            this.total_values = total_values;
-        }
 
-        public ArrayList<State> getStates() {
-            return states;
-        }
-
-        public void setStates(ArrayList<State> states) {
-            this.states = states;
-        }
     }
 
 
@@ -92,8 +95,8 @@ public class IndianDataWebService implements IndianDataWebInterface {
             @Override
             public void onResponse(Call<IndianDataWebService.Result> call, Response<IndianDataWebService.Result> response) {
                 if(response.isSuccessful()){
-                    ArrayList<State> states =  response.body().getStates();
-                    liveData.setValue(states);
+                    Log.i("Api",response.body().states.toString());
+                    //liveData.setValue(states);
                 }else {
                     APIError _apiError = new APIError();
                     _apiError.setCode(response.code());
